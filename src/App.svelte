6fce90d1230b554596aa6b9f7137839c2e1e0403
zaxiom13 <z.axiom.413@gmail.com>
2021-08-06 13:41:03 +0100

    minimisable

--- src/App.svelte
+++ src/App.svelte
@@ -1,151 +1,55 @@
 <script>
-  import Drag from "./lib/Drag.svelte";
-  let x = 100;
-  let y = 100;
-  let width = 200;
-  let height = 200;
-  let minWidth = 100;
-  let minHeight = 100;
-  let maxWidth = 300;
-  let maxHeight = 300;
-  let borderWidth = 20;
+  import Window from "./lib/Window.svelte";
 
-  let borderDown = false;
+  let windowTilesProps = [
+    { tileType: Window, visible: true },
+    { tileType: Window, visible: true },
+    { tileType: Window, visible: true },
+    { tileType: Window, visible: false },
+  ];
 
-  const setCurrentPosition = (/** @type {{ x: any; y: any; }} */ e) => {
-    current = {
-      x: e.x,
-      y: e.y,
-      width,
-      height,
-    };
-  };
-
-  let current = {
-    x,
-    y,
-    width,
-    height,
-  };
-  // function to add two nums
-
-  let borderstyles = new Array(4).fill(0);
-
-  const handleBorderDown = (num, e) => {
-    borderstyles[num] = "2";
-    borderDown = true;
-    setCurrentPosition(e);
+  const makeVisible = (index) => {
+    windowTilesProps[index].visible = true;
   };
 </script>
 
-<svelte:window
-  on:mouseup={() => {
-    borderstyles = borderstyles.map(() => 0);
-    borderDown = false;
-  }}
-  on:mousemove={(e) => {
-    
-    if (borderDown) {
-      let draggingHorizontal = Math.sign(e.movementX) > 0 ? "right" : "left";
-      let draggingVertical = Math.sign(e.movementY) > 0 ? "down" : "up";
-      if (borderstyles[3]) {
-        if (
-          (draggingHorizontal == "left" && width < maxWidth) ||
-          (draggingHorizontal == "right" && width > minWidth)
-        ) {
-          x += e.x - current.x;
-          width -= e.x - current.x;
-        } else {
-          borderstyles = new Array(4).fill(0);
-        }
-      }
-      if (borderstyles[1]) {
-        if (
-          (draggingHorizontal == "left" && width > minWidth) ||
-          (draggingHorizontal == "right" && width < maxWidth)
-        ) {
-          width += e.movementX;
-        } else {
-          borderstyles = new Array(4).fill(0);
-        }
-      }
-      if (borderstyles[0]) {
-        if (
-          (draggingVertical == "up" && height < maxHeight) ||
-          (draggingVertical == "down" && height > minHeight)
-        ) {
-          y += e.y - current.y;
-          height -= e.y - current.y;
-        } else {
-          borderstyles = new Array(4).fill(0);
-        }
-      }
-      if (borderstyles[2]) {
-        if (
-          (draggingVertical == "up" && height > minHeight) ||
-          (draggingVertical == "down" && height < maxHeight)
-        ) {
-          height += e.movementY;
-        } else {
-          borderstyles = new Array(4).fill(0);
-        }
-      }
-
-      current = { x, y, width, height };
-    }
-  }}
-/>
-
 <section class="main">
-  <div
-    style="top:{y}px; left:{x}px; width:{width}px; height:{height}px;  border-width: {borderstyles
-      .map((a) => a + 'px ')
-      .reduce((a, curr) => {
-        a += curr;
-        return a;
-      }, '')};
-      border-style: solid; 
-      border-color: white;"
-    class="window"
-    on:mousedown={(e) => {
-      if (e.offsetX < borderWidth) {
-        handleBorderDown(3, e);
-      }
-      if (e.offsetY < borderWidth) {
-        handleBorderDown(0, e);
-      }
-      if (e.offsetX > width - borderWidth) {
-        handleBorderDown(1, e);
-      }
-      if (e.offsetY > height - borderWidth) {
-        handleBorderDown(2, e);
-      }
-    }}
-  >
-    <div class="drag-container" on:mousedown|stopPropagation>
-      <Drag bind:x bind:y />
-    </div>
+  {#each windowTilesProps as props}
+    <svelte:component this={props.tileType} bind:visible={props.visible} />
+  {/each}
+  <div class="taskbar">
+    <div class="start-menu-button" />
+    {#each windowTilesProps as props, index}
+      {#if props.visible != true}
+        <div class="minimised-tile" on:click={() => makeVisible(index)}>
+          aadsdasd
+        </div>
+      {/if}
+    {/each}
   </div>
 </section>
 
 <style>
+  .minimised-tile {
+    width: 100px;
+    height: 100%;
+    margin-right: 30px;
+    background-color: bisque;
+  }
+  .taskbar {
+    width: 100%;
+    height: 7.5vh;
+    background-color: rgb(38, 38, 134);
+    display: flex;
+    flex-direction: row;
+  }
   .main {
     margin: 0;
     padding: 0;
     overflow-x: hidden;
     background-color: black;
     min-height: 100vh;
-  }
-  .window {
-    width: 200px;
-    height: 200px;
-    background-color: green;
-    position: absolute;
-  }
-  .drag-container {
-    width: 80%;
-    height: 20%;
-    margin: 10%;
-    position: absolute;
+    display: flex;
+    flex-direction: column-reverse;
   }
 </style>
